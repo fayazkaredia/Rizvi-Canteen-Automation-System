6a16fcdbdbee1347292354d513327a2c2ef41628
fayazkaredia <59962954+fayazkaredia@users.noreply.github.com>
2020-05-18 20:27:32 +0530

    Update c_neworder.aspx.cs

--- c_neworder.aspx.cs
+++ c_neworder.aspx.cs
@@ -13,7 +13,7 @@ namespace Canteen_Automation_System
 {
     public partial class c_neworder : System.Web.UI.Page
     {
-        SqlConnection conn = new SqlConnection(@"Data Source=43.255.152.26;Initial Catalog=ACanteen;Persist Security Info=True;User ID=ACanteen;Password=Do5oe2#5");
+        SqlConnection conn = new SqlConnection
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Session["canteen"] != "yes")
